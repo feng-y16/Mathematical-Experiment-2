ac280c0a2aed51e7933200ac7cb40aaf6190fdd9
Feng Yao <46351482+feng-y16@users.noreply.github.com>
2019-03-13 13:22:05 +0800

    Add files via upload

--- problem3.m
+++ problem3.m
@@ -7,4 +7,3 @@ competition_model_subplot(100,1000,1,1,100,100,0.5,2,5,20,'3(2)_3');
 competition_model_subplot(100,1000,1,1,100,100,1.5,0.7,10,10,'3(2)_4');
 competition_model_subplot(100,1000,1,1,100,100,0.8,0.7,10,10,'3(3)_1');
 competition_model_subplot(100,1000,1,1,100,100,1.5,1.7,10,10,'3(3)_2');
-competition_model_subplot(100,1000,0.0001,2000,100,100,0.5,2,10,10,'test1');
